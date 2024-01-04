5f0d4a03fac2ab75fc0f18e1f628e3fe9221d527
Shivank Singh Baghel <baghelshivank97@gmail.com>
2024-01-04 20:30:06 +0530

    Modified some styling of the home page and Added javascript for logout functionality

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+
+
+
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
 
@@ -11,3 +16,27 @@ import "controllers"
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+// // document.addEventListener("DOMContentLoaded", function () {
+//     const categorySelect = document.getElementById('logout-js');
+//     categorySelect.addEventListener('click', fetchSubcategories);
+//     function fetchSubcategories() {
+//         byebug
+//         console.log("updateScrollPoint triggered thjis is logout");
+//     }
+// // });
+// app/javascript/application.js
+
+// app/javascript/application.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const logoutButton = document.getElementById('logout-button');
+  
+    if (logoutButton) {
+      logoutButton.addEventListener('click', function() {
+        // Your JavaScript code here
+        window.alert('Logout button clicked!'); // Replace this with your actual JavaScript logic
+      });
+    }
+  });
+  
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,13 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+
+
+
+
+# Pin npm packages by running ./bin/importmap
+
+# pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
